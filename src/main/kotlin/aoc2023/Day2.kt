8d6dfa147e07561630af298cad371d33e9a0362b
Dallas Sanchez <dman012@gmail.com>
2023-12-02 13:53:43 -0500

    2023 day 2 - improved & simplified a lot with lookahead regex

--- src/main/kotlin/aoc2023/Day2.kt
+++ src/main/kotlin/aoc2023/Day2.kt
@@ -4,41 +4,41 @@ import util.*
 import kotlin.math.max
 
 private fun main() {
-    //pt 1 -
-    //pt 2 -
+//pt 1 answer: 2239
+//Pt 1 Time Taken: 0.03
+//pt 2 answer: 83435
+//Pt 2 Time Taken: 0.004
     TimeUtil.startClock(1, ::partOne)
     TimeUtil.startClock(2, ::partTwo)
 }
+
+private fun findRequiredDice(line: String, color: String): Int {
+    return Regex("\\d+(?= $color)").findAll(line).toList().maxOfOrNull { it.value.toInt() } ?: 0
+}
+
 
 private fun partOne(pt: Int = 1) {
     //Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
     //Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
     val input = InputUtil.readFileAsStringList("2023/day2/input.txt");
-    val games = input.map { line ->
-        line.split(Regex(": |; "))
-            .filterNot(String::isEmpty)
-    }
 
-    val gameList = mutableListOf<String>()
-    games.forEach { g ->
-        val maxMap = mutableMapOf<String, Int>()
-        g.drop(1).forEach {
-            it.split(", ").forEach {
-                val dice = it.split(" ");
-                maxMap.merge(dice[1], dice[0].toInt(), ::max)
-            }
-        }
+
+    val possibleGames = input.mapNotNull { line ->
         //Determine which games would have been possible if the bag had been loaded with only
         // 12 red cubes, 13 green cubes, and 14 blue cubes.
-        if (maxMap.getOrDefault("red", 0) <= 12 &&
-            maxMap.getOrDefault("green", 0) <= 13 &&
-            maxMap.getOrDefault("blue", 0) <= 14) {
-            gameList.add(g[0])
+        if (findRequiredDice(line, "red") <= 12 &&
+            findRequiredDice(line, "green") <= 13 &&
+            findRequiredDice(line, "blue") <= 14
+        ) {
+            //game is possible, grab the game # which is the first num in the line
+            Regex("\\d+").find(line)!!.value.toInt()
+        } else {
+            null //not possible so use null to drop this game from our mapping
         }
     }
 
     // What is the sum of the IDs of those games?
-    val answer = gameList.flatMap { it.findAllNumbers() }.sum()
+    val answer = possibleGames.sum()
     println("pt $pt answer: ${answer colorize ConsoleColor.BLUE_BRIGHT}")
 }
 
@@ -47,28 +47,17 @@ private fun partTwo(pt: Int = 2) {
     //Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
     //Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
     val input = InputUtil.readFileAsStringList("2023/day2/input.txt");
-    val games = input.map { line ->
-        line.split(Regex(": |; "))
-            .filterNot(String::isEmpty)
-    }
 
-    val powerList = mutableListOf<Double>()
-    games.forEach { g ->
-        val diceNeededMap = mutableMapOf<String, Double>()
-        g.drop(1).forEach {
-            it.split(", ").forEach {
-                val dice = it.split(" ");
-                diceNeededMap.merge(dice[1], dice[0].toDouble(), ::max)
-            }
-        }
-        //The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together.
-        val redNeeded = diceNeededMap.getOrDefault("red", 0.0);
-        val blueNeeded = diceNeededMap.getOrDefault("blue", 0.0);
-        val greenNeeded = diceNeededMap.getOrDefault("green", 0.0);
-        powerList.add(redNeeded * blueNeeded * greenNeeded)
+    //fewest number of cubes of each color
+    val powerValues = input.map { line ->
+        //The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together
+        val redNeeded = findRequiredDice(line, "red")
+        val greenNeeded = findRequiredDice(line, "green")
+        val blueNeeded = findRequiredDice(line, "blue")
+        redNeeded * greenNeeded * blueNeeded
     }
 
     // What is the sum of the power of these sets?
-    val answer = powerList.sum();
+    val answer = powerValues.sum();
     println("pt $pt answer: ${answer colorize ConsoleColor.BLUE_BRIGHT}")
 }
